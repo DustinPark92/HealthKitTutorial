7cc769a2cac74c5f4b97fd8cbede008f8f01a49b
Dustin <p4569zz@gmail.com>
2020-07-22 16:12:38 +0900

    Initial Commit

---
+++ HealthKitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthKitTutorial
+//
+//  Created by Dustin on 2020/07/22.
+//  Copyright © 2020 Dustin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
